45afb37fe972451a4d05a95ba71da1f0f952c594
CUI Wei <ghostplant@qq.com>
2017-04-10 16:50:34 +0800

    use overlay2 instead of btrfs

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 function compile {
 	cd $(dirname $0)/ubuntu-docker
 	sed -i 's/^ifeq.*nocheck.*$/ifeq (,0)/g' debian/rules
-	sed -i 's/dockerd .* -H/dockerd -s btrfs --icc=false --userland-proxy=false -H/g' contrib/init/systemd/docker.service
+	sed -i 's/dockerd .* -H/dockerd -s overlay2 --icc=false --userland-proxy=false -H/g' contrib/init/systemd/docker.service
 	sed -i 's/nuke-graph-directory.sh/purge-graph-directory.sh/g' debian/docker.io.prerm
 
 	dpkg-buildpackage -b || true
